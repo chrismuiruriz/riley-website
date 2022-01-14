ab3d56cfb345bb6ae039a6bd9d4bf337491489e5
chris muiruri <chrismuiruri007@gmail.com>
2022-01-14 18:27:01 +0300

    update the insurance page, added product filter based on url param

--- pages/solutions/[name].tsx
+++ pages/solutions/[name].tsx
@@ -8,14 +8,18 @@ import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import products from "../services/Products.json";
 
+const filterProducts = (type) => {
+  return products.filter(
+    (product) => product.type === type || product.type === "all"
+  );
+};
+
 export default function Service(): ReactElement {
   const router = useRouter();
 
   const { name } = router.query;
 
   const [activeSolution, setActiveSolution] = useState("Business");
-
-  const [productList, setProductList] = useState(products);
 
   //useEffect
   useEffect(() => {
@@ -26,8 +30,56 @@ export default function Service(): ReactElement {
     }
 
     //setProductList(products.filter((product) => product.type == activeSolution));
-    setProductList(products);
+    //setProductList(products);
   });
+
+  const p = filterProducts(name);
+
+  const BlockEl = () => {
+    let block = [];
+
+    p.map((item, idx) => {
+      block.push(
+        <div
+          key={idx}
+          className="max-w-sm rounded overflow-hidden shadow-lg bg-white mb-8 md:mb-0"
+        >
+          <div className="flex items-center p-6 pb-0">
+            <div className="text-6xl">
+              <FontAwesomeIcon icon={["fas", "shield-alt"]} />
+            </div>
+          </div>
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2 text-blue-900">{item.solutions}</div>
+            <p className="text-gray-700 text-base">{item.description}</p>
+          </div>
+          <div className="px-6 pt-2 pb-2">
+            {item.subSolutions.length > 0 && (
+              <Link
+                href={`/solutions/sub/${encodeURIComponent(item.solutions)}`}
+              >
+                <a className="inline-block py-1 text-sm font-semibold text-blue-400 mr-2 mb-2">
+                  <span className="mr-2 inline-block">View Products</span>
+                  <FontAwesomeIcon icon={["fas", "arrow-right"]} />
+                </a>
+              </Link>
+            )}
+
+            {item.subSolutions.length < 1 && (
+              <Link href="/#quote-form">
+                <a className="inline-block py-1 text-sm font-semibold text-blue-400 mr-2 mb-2">
+                  <span className="mr-2 inline-block">Get a quote</span>
+                  <FontAwesomeIcon icon={["fas", "arrow-right"]} />
+                </a>
+              </Link>
+            )}
+          </div>
+        </div>
+      );
+    });
+
+    return <>{block}</>;
+  };
 
   return (
     <>
@@ -59,7 +111,9 @@ export default function Service(): ReactElement {
         <div className="container grid md:grid-cols-3 md:gap-12">
           {/* Test Start */}
 
-          {productList.map((item, idx) => {
+          <BlockEl />
+
+          {/* {productList.map((item, idx) => {
             return (
               <div
                 key={idx}
@@ -99,7 +153,7 @@ export default function Service(): ReactElement {
                 </div>
               </div>
             );
-          })}
+          })} */}
 
           {/* Test End */}
         </div>
